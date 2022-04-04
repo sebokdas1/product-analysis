dfda89ce541e74ad451a12393c0e8ae4aaf3fce3
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-04 11:08:43 +0600

    review json added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,12 +4,17 @@ import './Home.css'
 const Home = () => {
     return (
         <div className='home'>
-            {/* <h2>Guiter Paradise</h2> */}
             <div className='home-container'>
-                <div>
-
+                <div className='heading'>
+                    <h3>Guiter Paradice</h3>
+                    <p>Guiter Paradise is the world's largest musical instrument retailer, with the greatest selection of guitars, basses, keyboards, drums and percussion, as well as studio gear, live sound and lighting equipment.</p>
+                    <p>We are happy to serve customers worldwide, and because of our strong relationships with the top musical instrument brands, we can offer you the best gear at a great low price.</p>
+                    <button className='live-btn'>LIVE DEMO</button>
                 </div>
                 <img src="https://fsa.zobj.net/crop.php?r=Zzb9QGPeQZkVbgbpUTuBxHYEhcxJu4REz4_kOhZa9GFO-loLMt0IKwAr6Xmr5v7c8VmXWbTTaWE4Y4uJakOEV2AQvpgDT4yqcvX8KbBfjrUVljoBU-Bzg8a2nPFPHZdu3jzJP6D6QrUE_cZ8oJO33BM_RPjRYu-cBnQeELQBo62mVx7-2ioEOESxOQ7luyMV9_lYxl1dqAkUp5bu" alt="" />
+            </div>
+            <div>
+                <h3>Customer Review()</h3>
             </div>
         </div>
     );
